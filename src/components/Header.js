1e4fba96c5ea329154cf4ab57bb1eca53d3d68da
Irina <webaribi@gmail.com>
2020-09-19 08:00:10 -0700

    Added nav menu the end of week 3

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+//import Header from './components/Header';
+
+// const Header = (props) =>{
+//     console.log(props)
+//     return(
+//         <header className="App">
+//             <h1>{props.title></h1>
+//             {
+//                 props.students.map((item) =>{
+//                     <div>{item}</div>
+//                 })
+//             }
+//         </header>
+//     );
+// };
+
+// export default Header;
+
+class Header extends Component {
+    render(){
+        return(<header>
+            <div>Amazon</div>
+        </header>)
+    
+}
