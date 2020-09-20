9ec81aaa5b5003c42a0cabbf244b20d3819b333c
Irina <webaribi@gmail.com>
2020-09-19 18:37:48 -0700

    Workshop-3 added breadcrumbs to About page

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,56 +1,68 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import React from "react";
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-function RenderCampsite({ campsite }) {
-  return (
-    <div className="col-md-5">
-      <Card>
-        <CardImg top src={campsite.image} alt={campsite.name} />
-        <CardBody>
-          <CardTitle>{campsite.name}</CardTitle>
-          <CardText>{campsite.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
-  );
-}
 
-function RenderComments({ comments }) {
-  if (comments) {
+  function RenderCampsite({campsite})  {
     return (
-      <div className="col-md-5 m-1">
-        <h4>Comments</h4>
-        {comments.map((comment) => (
-          <div className="comment-container mb-3">
-            <div>{comment.text}</div>
-            <div>
-              <strong>{comment.author}</strong>{" "}
-              {new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}
+      <div className="col-md-5">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  function RenderComments({comments}) {
+    if (comments) {
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => (
+            <div className="comment-container mb-3">
+              <div>{comment.text}</div>
+              <div>
+                <strong>{comment.author}</strong>{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comment.date)))}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    );
-  }
-  return <div />;
-}
-
-function CampsiteInfo(props) {
-  if (props.campsite) {
-    return (
-      <div className="container">
-        <div className="row">
-          <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.campsite.comments} />
+          ))}
         </div>
-      </div>
-    );
+      );
+    }
+    return <div />;
   }
-  return <div />;
+
+  function CampsiteInfo(props) {
+    if (props.campsite) {
+      return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>{props.campsite.name}</h2>
+                    <hr />
+                </div>
+            </div>
+            <div className="row">
+                <RenderCampsite campsite={props.campsite} />
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
+    );
+    }
+    return <div />;
 }
 
 export default CampsiteInfo;
